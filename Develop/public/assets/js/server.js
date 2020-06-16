f2a7b789eca02c365d8f04226dc19175e464b3b6
Alek <alekvalencia2535@gmail.com>
2020-06-15 22:31:23 -0700

    added server.js file with code for setting up server and some API calls. still need to make sure they work and add a delete API call. also added routes for html files. removed .DS_store so it can be gitignored.

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,65 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const db = require("../../../db/db.json");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Notes Taken (DATA)
+// =============================================================
+
+// API Routes
+// =============================================================
+
+// GET 
+app.get("/api/notes", (req, res) => res.json(fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+        throw err;
+    } else {
+        return data;
+    }
+})));
+
+// POST
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+  fs.appendFile(db, newNote, err => {
+      if (err) throw err;
+  });
+
+  res.json(fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+        throw err;
+    } else {
+        return data;
+    }
+}));
+});
+
+// HTML Routes
+// =============================================================
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../notes.html"));
+});
+
+// default to home
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
