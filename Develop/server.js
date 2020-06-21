07c0a25705adba75a5a914837e3bd0800e9d171d
Alek <alekvalencia2535@gmail.com>
2020-06-20 21:42:21 -0700

    created html route for homepage

--- Develop/server.js
+++ Develop/server.js
@@ -72,6 +72,10 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
